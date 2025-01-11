9e5d4adafa8568c4560e8270768cc72e3e23f7f7
Marcelo Neves <141940013+marceloairesdasneves@users.noreply.github.com>
2025-01-11 14:18:48 +0000

    test

--- Olamundo.java
+++ Olamundo.java
@@ -1,5 +1,6 @@
 public class Olamundo {
     public static void main(String[] args) {
-        System.out.println("Ol  , Mundo constru do com sucesso!");
+        System.out.println("Ola Mundo, voce acabou de construído com sucesso!");
+        System.out.println("Olá, Mundo, novo Marcelo. Agora é Java!");
     }
 }
