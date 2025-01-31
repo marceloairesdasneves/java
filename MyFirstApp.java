d26340681374fb66efeeaa79b8b26079ea154606
marcelo aires das neves <mneves230@gmail.com>
2025-01-31 16:17:16 -0300

    `reading the Head First JAVA`

---
+++ MyFirstApp.java
@@ -0,0 +1,7 @@
+public class MyFirstApp{
+/*************  ✨ Codeium Command 🌟  *************/
+    public static void main(String[] args) {
+        System.out.println("I Rule!");
+        System.out.println("The World");
+    }
+}
