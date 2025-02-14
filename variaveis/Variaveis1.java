b276a5eb4dc163d4422fb66f8deacd83a8272beb
Marcelo Neves <141940013+marceloairesdasneves@users.noreply.github.com>
2025-02-14 00:00:04 +0000

    test

---
+++ variaveis/Variaveis1.java
@@ -0,0 +1,10 @@
+package Variaveis1;
+
+public class Variaveis1 {
+    public static void main(String[] args){
+        int idade = 20;
+        String nome = "Lucas";
+        System.out.println("A idade de " + nome + " e " + idade + " anos");   
+           
+    }
+}
